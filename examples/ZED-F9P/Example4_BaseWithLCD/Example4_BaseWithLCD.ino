71ee3a9a23e6ddb30e6895a4785433751f943447
nseidle <nathan@sparkfun.com>
2019-01-14 11:32:30 -0700

    Adding Base with LCD example

--- examples/ZED-F9P/Example4_BaseWithLCD/Example4_BaseWithLCD.ino
+++ examples/ZED-F9P/Example4_BaseWithLCD/Example4_BaseWithLCD.ino
@@ -20,36 +20,53 @@
 
   Hardware Connections:
   Plug a Qwiic cable into the GPS and a BlackBoard
+  Plug a SerLCD onto the Qwiic bus
   If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Open the serial monitor at 115200 baud to see the output
+  Watch the output on the LCD or open the serial monitor at 115200 baud to see the output
 */
+
+#define STAT_LED 13
 
 #include <Wire.h> //Needed for I2C to GPS
 
-#include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_Ublox_GPS
+#include "SparkFun_Ublox_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_Ublox_GPS
 SFE_UBLOX_GPS myGPS;
+
+#include <SerLCD.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
+SerLCD lcd; // Initialize the library with default I2C address 0x72
 
 void setup()
 {
   Serial.begin(115200);
   while (!Serial); //Wait for user to open terminal
-  Serial.println("Ublox Base station example");
+  Serial.println("Ublox GPS I2C Test");
 
   Wire.begin();
-  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
 
-  if (myGPS.begin() == false) //Connect to the Ublox module using Wire port
+  pinMode(STAT_LED, OUTPUT);
+  digitalWrite(STAT_LED, LOW);
+
+  lcd.begin(Wire); //Set up the LCD for Serial communication at 9600bps
+  lcd.setBacklight(0x4B0082); //indigo, a kind of dark purplish blue
+  lcd.clear();
+  lcd.print(F("LCD Ready"));
+
+  myGPS.begin(Wire);
+  if (myGPS.isConnected() == false)
   {
     Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    lcd.setCursor(0, 1);
+    lcd.print(F("No GPS detected"));
     while (1);
   }
 
+  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+
+  lcd.setCursor(0, 1);
+  lcd.print("GPS Detected");
+
   myGPS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
   myGPS.saveConfiguration(); //Save the current settings to flash and BBR
-
-  while (Serial.available()) Serial.read(); //Clear any latent chars in serial buffer
-  Serial.println("Press any key to send commands to begin Survey-In");
-  while (Serial.available() == 0) ; //Wait for user to press a key
 
   boolean response = true;
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_I2C, 1); //Enable message 1005 to output through I2C port, message every second
@@ -58,18 +75,13 @@ void setup()
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1094, COM_PORT_I2C, 1);
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1124, COM_PORT_I2C, 1);
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
-
-  //Use COM_PORT_UART1 for the above six messages to direct RTCM messages out UART1
-  //COM_PORT_UART2, COM_PORT_USB, COM_PORT_SPI are also available
-  //For example: response &= myGPS.enableRTCMmessage(UBX_RTCM_1005, COM_PORT_UART1, 10);
-
   if (response == true)
   {
-    Serial.println("RTCM messages enabled");
+    Serial.println(F("RTCM messages enabled"));
   }
   else
   {
-    Serial.println("RTCM failed to enable. Are you sure you have an NEO-M8P?");
+    Serial.println(F("RTCM failed to enable. Are you sure you have an NEO-M8P?"));
     while (1); //Freeze
   }
 
@@ -77,72 +89,89 @@ void setup()
   response = myGPS.getSurveyStatus(2000); //Query module for SVIN status with 2000ms timeout (request can take a long time)
   if (response == false)
   {
-    Serial.println("Failed to get Survey In status");
+    Serial.println(F("Failed to get Survey In status"));
     while (1); //Freeze
   }
 
   if (myGPS.svin.active == true)
   {
-    Serial.print("Survey already in progress.");
+    Serial.print(F("Survey already in progress."));
+    lcd.setCursor(0, 2);
+    lcd.print(F("Survey already going"));
   }
   else
   {
     //Start survey
-    //The ZED-F9P is slightly different than the NEO-M8P. See the Integration manual 3.5.8 for more info.
-    //response = myGPS.enableSurveyMode(300, 2.000); //Enable Survey in on NEO-M8P, 300 seconds, 2.0m
     response = myGPS.enableSurveyMode(60, 5.000); //Enable Survey in, 60 seconds, 5.0m
     if (response == false)
     {
-      Serial.println("Survey start failed");
+      Serial.println(F("Survey start failed"));
+      lcd.setCursor(0, 3);
+      lcd.print(F("Survey start failed"));
       while (1);
     }
-    Serial.println("Survey started. This will run until 60s has passed and less than 5m accuracy is achieved.");
+    Serial.println(F("Survey started. This will run until 300s has passed and less than 2m accuracy is achieved."));
   }
 
-  while(Serial.available()) Serial.read(); //Clear buffer
-  
+  while (Serial.available()) Serial.read(); //Clear buffer
+
+  lcd.clear();
+  lcd.print(F("Survey in progress"));
+
   //Begin waiting for survey to complete
   while (myGPS.svin.valid == false)
   {
-    if(Serial.available())
+    if (Serial.available())
     {
       byte incoming = Serial.read();
-      if(incoming == 'x')
+      if (incoming == 'x')
       {
         //Stop survey mode
         response = myGPS.disableSurveyMode(); //Disable survey
-        Serial.println("Survey stopped");
+        Serial.println(F("Survey stopped"));
         break;
       }
     }
-    
+
     response = myGPS.getSurveyStatus(2000); //Query module for SVIN status with 2000ms timeout (req can take a long time)
     if (response == true)
     {
-      Serial.print("Press x to end survey - ");
-      Serial.print("Time elapsed: ");
+      Serial.print(F("Press x to end survey - "));
+      Serial.print(F("Time elapsed: "));
       Serial.print((String)myGPS.svin.observationTime);
 
-      Serial.print(" Accuracy: ");
+      lcd.setCursor(0, 1);
+      lcd.print(F("Elapsed: "));
+      lcd.print((String)myGPS.svin.observationTime);
+
+      Serial.print(F(" Accuracy: "));
       Serial.print((String)myGPS.svin.meanAccuracy);
       Serial.println();
+
+      lcd.setCursor(0, 2);
+      lcd.print(F("Accuracy: "));
+      lcd.print((String)myGPS.svin.meanAccuracy);
     }
     else
     {
-      Serial.println("SVIN request failed");
+      Serial.println(F("SVIN request failed"));
     }
 
     delay(1000);
   }
-  Serial.println("Survey valid!");
+  Serial.println(F("Survey valid!"));
 
-  Serial.println("Base survey complete! RTCM now broadcasting.");
+  Serial.println(F("Base survey complete! RTCM now broadcasting."));
+  lcd.clear();
+  lcd.print(F("Transmitting RTCM"));
 }
 
 void loop()
 {
   myGPS.checkUblox(); //See if new data is available. Process bytes as they come in.
 
+  //Do anything you want. Call checkUblox() every second. NEO-M8P-2 has TX buffer of 4k bytes.
+  
   delay(250); //Don't pound too hard on the I2C bus
 }
 
