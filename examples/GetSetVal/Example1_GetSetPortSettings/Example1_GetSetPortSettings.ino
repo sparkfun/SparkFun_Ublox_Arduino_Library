0fb2fcf4da2e53bc5f571af5521e5705c3d6c992
Nathan Seidle <nathan@sparkfun.com>
2020-10-23 12:47:41 -0600

    Update Example1_GetSetPortSettings.ino

--- examples/GetSetVal/Example1_GetSetPortSettings/Example1_GetSetPortSettings.ino
+++ examples/GetSetVal/Example1_GetSetPortSettings/Example1_GetSetPortSettings.ino
@@ -6,7 +6,7 @@
   License: MIT. See license file for more information but you can
   basically do whatever you want with this code.
 
-  This example shows how to query a u-blox module for its UART1 settings and
+  This example shows how to query a Ublox module for its UART1 settings and
   then change them if the settings aren't what we want.
 
   Note: getVal/setVal/delVal are only support in u-blox protocol versions 27 and higher.
@@ -33,16 +33,18 @@ void setup()
   Serial.begin(115200);
   while (!Serial)
     ; //Wait for user to open terminal
-  Serial.println("SparkFun u-blox Example");
+  Serial.println("SparkFun Ublox Example");
 
   Wire.begin();
 
   if (myGPS.begin() == false) //Connect to the Ublox module using Wire port
   {
-    Serial.println(F("u-blox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
     while (1)
       ;
   }
+
+  bool response = true;
 
   //Read the settings from RAM (what the module is running right now, not BBR, Flash, or default)
   uint8_t currentUART1Setting_ubx = myGPS.getVal8(UBLOX_CFG_UART1INPROT_UBX);
@@ -62,7 +64,6 @@ void setup()
     Serial.println("Updating UART1 configuration");
 
     //setVal sets the values for RAM, BBR, and Flash automatically so no .saveConfiguration() is needed
-    bool response = true;
     response &= myGPS.setVal8(UBLOX_CFG_UART1INPROT_UBX, 1);    //Enable UBX on UART1 Input
     response &= myGPS.setVal8(UBLOX_CFG_UART1INPROT_NMEA, 1);   //Enable NMEA on UART1 Input
     response &= myGPS.setVal8(UBLOX_CFG_UART1INPROT_RTCM3X, 0); //Disable RTCM on UART1 Input
@@ -73,7 +74,23 @@ void setup()
       Serial.println("SetVal succeeded");
   }
   else
-    Serial.println("Nothing to change");
+    Serial.println("No port change needed");
+
+  //Change speed of UART2
+  uint32_t currentUART2Baud = myGPS.getVal32(UBLOX_CFG_UART2_BAUDRATE);
+  Serial.print("currentUART2Baud: ");
+  Serial.println(currentUART2Baud);
+
+  if (currentUART2Baud != 57600)
+  {
+    response &= myGPS.setVal32(UBLOX_CFG_UART2_BAUDRATE, 57600);
+    if (response == false)
+      Serial.println("SetVal failed");
+    else
+      Serial.println("SetVal succeeded");
+  }
+  else
+    Serial.println("No baud change needed");
 
   Serial.println("Done");
 }
