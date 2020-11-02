9ccdb9f7492ab65d1d36d4ef06a60d479cc6e634
vid <vid@irnas.eu>
2020-11-02 15:52:40 +0100

    changed prints

--- examples/Example_Zephyr/src/main.c
+++ examples/Example_Zephyr/src/main.c
@@ -17,7 +17,6 @@
 #include <device.h>
 #include <drivers/i2c.h>
 #include <errno.h>
-#include <logging/log.h>
 #include <zephyr.h>
 #include <zephyr/types.h>
 
@@ -25,8 +24,6 @@
 
 
 #define I2C_DEV "I2C_0"
-
-LOG_MODULE_REGISTER(zephyr_main);      // init logging
 
 struct device *gpio_dev;
 struct device *i2c_dev;
@@ -39,7 +36,7 @@ uint8_t init_gpio(void) {
 	const char* gpioName = "GPIO_0";
 	gpio_dev = device_get_binding(gpioName);
 	if (gpio_dev == NULL) {
-		LOG_ERR("Could not get %s device", gpioName);
+		printk("Error: Could not get %s device\n", gpioName);
 		return -EIO;
 	}
     int err = set_gpio_dev(gpio_dev);
@@ -53,12 +50,12 @@ uint8_t init_i2c(void) {
 	i2c_dev = device_get_binding(I2C_DEV);
     if (!i2c_dev)
     {
-        LOG_ERR("I2C_0 error");
+        printk("I2C_0 error\n");
         return -1;
     }
     else
     {
-        LOG_INF("I2C_0 Init OK");
+        printk("I2C_0 Init OK\n");
         return 0;
     }
 }
@@ -66,7 +63,7 @@ uint8_t init_i2c(void) {
 uint8_t init_gps(void) {
 	if (gps_begin(i2c_dev) != 0)
   	{
-    	LOG_ERR("Ublox GPS init error!");
+    	printk("Ublox GPS init error!\n");
         return -1;
   	}
     return 0;
@@ -74,7 +71,7 @@ uint8_t init_gps(void) {
 
 
 void main(void) {
-	LOG_INF("Ublox Zephyr example");
+	printk("Ublox Zephyr example\n");
 
 	int err;
     err = init_gpio();
